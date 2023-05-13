0424383e08ebb99fd17809caf5abcb615df04e95
Nikolay Sholokhov <devilzick@gmail.com>
2023-05-13 11:02:47 +0300

    modified Test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    void shouldSetMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
